66d5e353a042c49761c7a91a23d5247e80b5350c
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-05 16:24:04 -0300

    feat(mongoose): setup mongoose db connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
-});
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to database ^ listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
