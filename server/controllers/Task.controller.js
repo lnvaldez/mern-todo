20b04ded6c8cdadf8c3d7433d4f4a38ce5b09bc5
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-06 18:33:03 -0300

    feat(tasks): function to insert a task

--- server/controllers/Task.controller.js
+++ server/controllers/Task.controller.js
@@ -2,10 +2,21 @@ const Task = require("../models/Task.model");
 const mongoose = require("mongoose");
 
 // Get all tasks
-const getTasks = async (req, res) => {
+const find = async (req, res) => {
   try {
     const tasks = await Task.find();
     res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Create new task
+const insert = async (req, res) => {
+  const title = req.body;
+  try {
+    await Task.insert(title);
+    res.status(200).json({ message: "Task created" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
