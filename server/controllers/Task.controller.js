941bfa539f88a469393b93bc9caf7e8adb523599
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-06 18:29:31 -0300

    feat(task): function to retrieve all tasks

---
+++ server/controllers/Task.controller.js
@@ -0,0 +1,12 @@
+const Task = require("../models/Task.model");
+const mongoose = require("mongoose");
+
+// Get all tasks
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
