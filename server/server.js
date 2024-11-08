e538de3ab1c1acfbdd65b1fe712a458fb76245ef
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-08 13:12:57 -0300

    Use task routes on express app

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Routes
+const taskRoutes = require("./routes/Task.routes");
+
 const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/tasks", taskRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
