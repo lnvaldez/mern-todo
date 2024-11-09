2f0150d2fc382ef6d910513cb93c3b51dd6d5d27
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-09 15:44:36 -0300

    feat(user): user controller function to register

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -4,3 +4,15 @@ const jwt = require("jsonwebtoken");
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
 };
+
+const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    await User.register(username, email, password);
+
+    res.status(200).json({ email });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
