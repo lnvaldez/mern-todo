4df221968024e21c62e55eef176cd97ccd914e06
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-09 15:41:02 -0300

    feat(user): function to generate a jwt token

---
+++ server/controllers/User.controller.js
@@ -0,0 +1,6 @@
+const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
