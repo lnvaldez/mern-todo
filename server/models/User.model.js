490fc0cea553dc3f24c876e5b3e44c30f15294b4
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-09 15:04:44 -0300

    feat(user): user data validation logic

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -17,5 +18,33 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.statics.register = async function (username, email, password) {
+  // Validation
+
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
+  const usernameExists = await this.findOne({ username });
+
+  if (usernameExists) {
+    throw Error("Username already in use");
+  }
+
+  const emailExists = await this.findOne({ email });
+
+  if (emailExists) {
+    throw Error("Email already in use");
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
