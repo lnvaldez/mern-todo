609b283a66814788789f3723c68edb925c2a0396
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-09 15:08:25 -0300

    feat(user): complete registration function logic

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const validator = require("validator");
 
 const Schema = mongoose.Schema;
@@ -45,6 +46,13 @@ userSchema.statics.register = async function (username, email, password) {
   if (emailExists) {
     throw Error("Email already in use");
   }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ username, email, password: hash });
+
+  return user;
 };
 
 module.exports = mongoose.model("User", userSchema);
