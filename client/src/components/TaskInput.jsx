4c3d9cdb4990a541513551571f9c240d92224953
Lucas Valdez <lnvaldez24@gmail.com>
2025-01-26 11:11:44 -0300

    feat: ant-design gradient button

--- client/src/components/TaskInput.jsx
+++ client/src/components/TaskInput.jsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
 import { useTasksContext } from "../hooks/useTasksContext";
-import { Input, Button, Form } from "antd";
+import { Input, Button, Form, ConfigProvider } from "antd";
+import { createStyles } from "antd-style";
 import { PlusOutlined } from "@ant-design/icons";
 
+const useStyle = createStyles(({ prefixCls, css }) => ({
+  linearGradientButton: css`
+    &.${prefixCls}-btn-primary:not([disabled]):not(
+        .${prefixCls}-btn-dangerous
+      ) {
+      > span {
+        position: relative;
+      }
+
+      &::before {
+        content: "";
+        background: linear-gradient(135deg, #6253e1, #04befe);
+        position: absolute;
+        inset: -1px;
+        opacity: 1;
+        transition: all 0.3s;
+        border-radius: inherit;
+      }
+
+      &:hover::before {
+        opacity: 0;
+      }
+    }
+  `,
+}));
+
 const TaskInput = () => {
+  const { styles } = useStyle();
   const { dispatch } = useTasksContext();
   const [error, setError] = useState(null);
   const [form] = Form.useForm();
@@ -35,21 +63,31 @@ const TaskInput = () => {
   return (
     <div style={{ maxWidth: "600px", margin: "20px auto" }}>
       <h1>Add Todo</h1>
-      <Form form={form} onFinish={handleSubmit}>
-        <Form.Item
-          name="title"
-          rules={[{ required: true, message: "Please enter a task" }]}
-        >
-          <Input
-            placeholder="Add task"
-            suffix={
-              <Button type="primary" htmlType="submit" icon={<PlusOutlined />}>
-                Add
-              </Button>
-            }
-          />
-        </Form.Item>
-      </Form>
+      <ConfigProvider
+        button={{
+          className: styles.linearGradientButton,
+        }}
+      >
+        <Form form={form} onFinish={handleSubmit}>
+          <Form.Item
+            name="title"
+            rules={[{ required: true, message: "Please enter a task" }]}
+          >
+            <Input
+              placeholder="Add task"
+              suffix={
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  icon={<PlusOutlined />}
+                >
+                  Add
+                </Button>
+              }
+            />
+          </Form.Item>
+        </Form>
+      </ConfigProvider>
       {error && <div style={{ color: "red" }}>{error}</div>}
     </div>
   );
